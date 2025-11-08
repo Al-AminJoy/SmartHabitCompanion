0e4540b20685c0e2fcdb144eba1b496913caa73c
Al-AminJoy <alaminislam3555@gmail.com>
2025-11-08 23:36:03 +0600

    Grid Added

--- app/src/main/java/com/alamin/smarthabitcompanion/ui/presentation/habits/HabitsScreeen.kt
+++ app/src/main/java/com/alamin/smarthabitcompanion/ui/presentation/habits/HabitsScreeen.kt
@@ -1,10 +1,16 @@
 package com.alamin.smarthabitcompanion.ui.presentation.habits
 
 import android.widget.Toast
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
@@ -12,8 +18,10 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.alamin.smarthabitcompanion.core.utils.AppConstants
 import com.alamin.smarthabitcompanion.ui.navigation.NavigationDestinations
 import com.alamin.smarthabitcompanion.ui.presentation.components.HabitItem
 import com.alamin.smarthabitcompanion.ui.presentation.main.MainActivityViewModel
@@ -36,8 +44,11 @@ fun HabitsScreen(
     Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
 
         if (uiState.habits.isNotEmpty()){
-            LazyColumn {
-                items(uiState.habits){ habit ->
+            LazyVerticalGrid(columns = GridCells.Fixed(2), contentPadding = PaddingValues(
+                (AppConstants.APP_MARGIN).dp), horizontalArrangement = Arrangement.spacedBy(
+                AppConstants.APP_MARGIN.dp), verticalArrangement = Arrangement.spacedBy((AppConstants.APP_MARGIN).dp)) {
+                items(uiState.habits.size){ index ->
+                    val habit = uiState.habits[index]
                     HabitItem(habit,Modifier.fillMaxWidth(),toHabitDetails = toHabitDetails)
                 }
             }
